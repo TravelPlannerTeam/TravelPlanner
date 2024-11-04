597880a0f473753ecbd808cd3c772c18ac98c82a
Moaz Mubaydin <mmubaydin@gmail.com>
2024-11-04 15:45:44 +0100

    Added weather component functionality

---
+++ src/components/Weather.jsx
@@ -0,0 +1,51 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export default function Weather({ city }) {
+  const [weatherData, setWeatherData] = useState(null);
+
+  const Apikey = "078f0f6d0f3a4805b3660728af344b64";
+  const widgetStyles = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: "10px",
+    borderRadius: "10px",
+    backgroundColor: "#4a90e2",
+    color: "#ffffff",
+    width: "150px",
+    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+  };
+
+  const tempStyles = {
+    fontSize: "2rem",
+    fontWeight: "bold",
+    margin: "5px 0",
+  };
+
+  const additionalInfoStyles = {
+    fontSize: "0.8rem",
+    marginTop: "5px",
+  };
+  useEffect(() => {
+    axios
+      .get(`https://api.weatherbit.io/v2.0/current?city=${city}&key=${Apikey}`)
+      .then((response) => {
+        setWeatherData(response.data.data[0]);
+        console.log(response);
+      })
+      .catch((e) => console.log("Error fetching Weather", e));
+  }, [city]);
+  if (weatherData !== null)
+    return (
+      <div style={widgetStyles}>
+        <h3>{weatherData.city_name}</h3>
+        <div style={tempStyles}>{weatherData.temp}°C</div>
+        <p>{weatherData.weather.description}</p>
+        <div style={additionalInfoStyles}>
+          <p>Wind: {weatherData.wind_spd} m/s</p>
+          <p>Humidity: {weatherData.rh}%</p>
+        </div>
+      </div>
+    );
+}
