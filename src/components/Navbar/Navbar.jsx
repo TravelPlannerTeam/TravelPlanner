aafd86db74989357927ab482f769d7ea366c5f71
nathidaum <daumnathi@gmail.com>
2024-11-05 19:28:27 +0100

    Implement Logout button & functionality in Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,28 @@
-import { Button } from '@mantine/core';
+import { Button } from "@mantine/core";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
 
 import "./navbar.css";
 import logo from "../../assets/logo.png";
 
 const Navbar = ({ openForm, callBackToFilterPlans, query }) => {
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+  
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/login"); // Redirect to login page after logout
+    } catch (e) {
+      console.log("Failed to log out", e);
+    }
+  };
+
   return (
     <nav id="navbar">
       <div className="nav-left">
         <img src={logo} alt="logo" className="logo" />
-        <h1>TravelPlanner</h1>
+        <h1 className="title">TravelPlanner</h1>
       </div>
       <div className="nav-right">
         <div id="navbar-search">
@@ -20,7 +34,26 @@ const Navbar = ({ openForm, callBackToFilterPlans, query }) => {
           />
         </div>
         <div>
-        <Button onClick={openForm} variant="filled" color="yellow" size="lg" radius="md">Create new plan</Button>;
+          <Button
+            onClick={openForm}
+            variant="filled"
+            color="yellow"
+            size="lg"
+            radius="md"
+          >
+            Create new plan
+          </Button>
+        </div>
+        <div>
+          <Button
+            onClick={handleLogout}
+            variant="subtle"
+            color="yellow"
+            size="lg"
+            radius="md"
+          >
+            Logout
+          </Button>
         </div>
       </div>
     </nav>
