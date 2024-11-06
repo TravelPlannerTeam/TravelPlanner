a85c7d659765229b7a3914138f627f80bbac42b0
nathidaum <daumnathi@gmail.com>
2024-11-06 10:12:19 +0100

    Use .env for weather api

--- src/assets/API_URL.js
+++ src/assets/API_URL.js
@@ -1,3 +1,7 @@
 export const API_URL = import.meta.env.VITE_API_URL;
+
 export const UNSPLASH_API_URL = import.meta.env.VITE_UNSPLASH_API_URL;
 export const UNSPLASH_API_TOKEN = import.meta.env.VITE_UNSPLASH_API_TOKEN;
+
+export const WEATHER_API_URL = import.meta.env.VITE_WEATHER_API_URL;
+export const WEATHER_API_TOKEN = import.meta.env.VITE_WEATHER_API_TOKEN;
