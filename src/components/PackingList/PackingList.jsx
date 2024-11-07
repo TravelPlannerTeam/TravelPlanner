f895d7c5619a183e8c2ce68d7013e77a41fe9026
nathidaum <daumnathi@gmail.com>
2024-11-07 14:56:21 +0100

    Implement checkboxes for packing list

--- src/components/PackingList/PackingList.jsx
+++ src/components/PackingList/PackingList.jsx
@@ -1,45 +1,54 @@
-import {
-  Card,
-  Text,
-  Button,
-  Group,
-  List,
-  ActionIcon,
-  Loader,
-} from "@mantine/core";
+import { Text, Checkbox, Group, List, ActionIcon, Loader } from "@mantine/core";
 import { IconTrash } from "@tabler/icons-react";
+import { useState, useEffect } from "react";
 
 import "./packing-list.css";
 
 const PackingList = ({ packingList, callBackToDelete, callBackToUpdate }) => {
+  const [checkedItems, setCheckedItems] = useState({});
+
+  // Initialize checkedItems with packingList data on component mount
+  useEffect(() => {
+    if (packingList) {
+      const initialCheckedState = packingList.reduce((acc, item) => {
+        acc[item.id] = false;
+        return acc;
+      }, {});
+      setCheckedItems(initialCheckedState);
+    }
+  }, [packingList]);
+
   if (!packingList) return <Loader color="yellow" size="xl" type="dots" />;
+  
   return (
     <List>
       {packingList.map((item) => {
         return (
           <div key={item.id}>
-            <List.Item className="packing-list-item">
-              <Group
-                justify="flex-start"
-                mt="md"
-                mb="xs"
-                className="packing-list-content"
-              >
-                <Group spacing="xs">
-                  <Text fw={600}>{item.name}</Text>
-                  {/* <Text size="sm">{item.type}</Text> */}
-                </Group>
+            <Group className="packing-list-content">
+              <Checkbox
+                checked={checkedItems[item.id] || false}
+                label={item.name}
+                color="yellow"
+                onChange={(event) =>
+                  setCheckedItems((prev) => ({
+                    ...prev,
+                    [item.id]: event.target.checked,
+                  }))
+                }
+              />
 
-                <ActionIcon
-                  onClick={() => callBackToDelete(item.id)}
-                  className="transparent-action-icon"
-                  radius="md"
-                  size={36}
-                >
-                  <IconTrash className="delete" stroke={1} />
-                </ActionIcon>
-              </Group>
-            </List.Item>
+              <ActionIcon
+                onClick={() => callBackToDelete(item.id)}
+                className="packing-transparent-action-icon"
+                radius="md"
+                size={36}
+              >
+                <IconTrash className="delete" stroke={1} />
+              </ActionIcon>
+            </Group>
+
+            {/* // </List.Item> */}
           </div>
         );
       })}
